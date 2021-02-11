d9d5184de28a0847573626e1e7f4c89d8477df34
Blazej Wielk <bwielk@gmail.com>
2021-02-11 17:31:47 +0000

    turning vars into json objects for loators

--- cypress/integration/login/login_spec.js
+++ cypress/integration/login/login_spec.js
@@ -3,20 +3,15 @@ const PASSWORD = "test_cypress_1"
 const EMAIL = "test_cypress_1@test.com"
 const URL = "https://react-redux.realworld.io/#/login"
 
-//locators login page
-const EMAIL_INPUT = 'input[placeholder="Email"]'
-const PASSWORD_INPUT = 'input[placeholder="Password"]'
-const LOGIN_BUTTON = 'button[type="submit"]'
-//locators user page
-const SETTINGS_LINK = 'a[href="#settings"]'
+const locators = require("../../fixtures/locators.json")
 
 describe ("Login", function(){
     it("Sign in", function(){
         cy.visit(URL)
-        cy.get(EMAIL_INPUT).type(EMAIL)
-        cy.get(PASSWORD_INPUT).type(PASSWORD)
-        cy.get(LOGIN_BUTTON).click()
-        cy.get(SETTINGS_LINK).should('be.visible')
-        cy.get(LOGIN_BUTTON).should('not.exist')
+        cy.get(locators.login_page.email_input).type(EMAIL)
+        cy.get(locators.login_page.password_input).type(PASSWORD)
+        cy.get(locators.login_page.login_button).click()
+        cy.get(locators.user_page.settings_link).should('be.visible')
+        cy.get(locators.login_page.login_button).should('not.exist')
     })
 })
